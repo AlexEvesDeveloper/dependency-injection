041b396fbd71125bb801d4660ccb8497436b1bfa
AlexEvesDeveloper <alexevesdeveloper@gmail.com>
2014-08-14 12:18:24 +0100

    Solid example of DI design pattern with a Container, before introduction of SF Container

--- src/Project/Classes/DependencyInjection/Container.php
+++ src/Project/Classes/DependencyInjection/Container.php
@@ -7,6 +7,10 @@ use Project\Classes\Readers\JsonReader;
 
 class Container
 {
+	// an array of existing instances, to ensure we don't recreate service objects that have already been created
+	static protected $instances = array();
+	// client code interacts with this container class directly, so any parameters that the service objects want, should be
+	// passed into the Container
 	protected $parameters = array();
 
 	public function __construct(array $parameters = array())
@@ -14,19 +18,31 @@ class Container
 		$this->parameters = $parameters;
 	}
 
-	public function getJsonReader()
+	// returns a service object, which can then be injected into any other classes in this container that depend on it
+	public function getReader()
 	{
-		return new JsonReader(array(
+		// a bit of flexibility, you pass in the name of the Concrete Reader class when creating the Container
+		$class = $this->parameters['reader.class'];
+
+		$reader = new $class(array(
 			'url' => $this->parameters['reader.url'],
 			'max' => $this->parameters['reader.max'],
 		));
+
+		return $reader;
 	}
 
+	// returns a service object, which is dependent on the Reader class above. 
 	public function getReaderManager()
 	{
-		$rm = new ReaderManager();
-		$rm->setReaderType($this->getJsonReader());
+		// only ever want one instance...
+		if(isset(self::$instances['reader_manager'])){
+			return self::$instances['reader_manager'];
+		}
 
-		return $rm;
+		$rm = new ReaderManager();
+		$rm->setReaderType($this->getReader());
+
+		return self::$instances['reader_manager'] = $rm;
 	}
 }
--- web/index.php
+++ web/index.php
@@ -5,11 +5,16 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Project\Classes\DependencyInjection\Container;
 
 $container = new Container(array(
+	'reader.class' => 'Project\Classes\Readers\JsonReader',
 	'reader.url' => 'http://api.indeed.com/ads/apisearch?publisher=3880066252893078&q=graduate&l=&sort=1',
 	'reader.max' => 10,
 ));
 
-$rm = $container->getReaderManager();
+// get a ReaderManager, with it's dependencies all handled by the Container
+$rm1 = $container->getReaderManager();
 
-print '<pre>';
-print_r($rm);
+// get the VERY SAME instance, doesn;t create more than one instance
+$rm2 = $container->getReaderManager();
+
+// prove they are the same
+print $rm1 === $rm2; // true
